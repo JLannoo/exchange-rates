96f82d0b3af20e9f4b22da3d3c990e66557fc4d3
JLannoo <julianlannoo@gmail.com>
2021-10-05 16:22:46 -0300

    Migrated API again

--- docs/main.js
+++ docs/main.js
@@ -1,34 +1,49 @@
 const $list = document.querySelector("#list");
+const $date = document.querySelector("#date");
 const $selectContainer = document.querySelector("#selectContainer");
-const $currencySelect = document.querySelector("#currencyType");
-const $coinSelect = document.querySelector("#coin");
+const $baseCurrencySelect = document.querySelector("#baseCurrency");
+const $findCurrencySelect = document.querySelector("#findCurrency");
+
+const now = new Date();
+const y = now.getFullYear();
+const m = String(now.getMonth()+1).padStart(2,0);
+const d = String(now.getDate()).padStart(2,0);
+const today = y+"-"+m+"-"+d;
+$date.value = today;
+$date.max = today;
 
 (async function getCodes(){
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/8697e71e86e8d0f6023edebf/codes`);
-    const codes = await response.json();
+    const response = await fetch(`https://api.exchangerate.host/symbols`);
+    const data = await response.json();
 
-    for(code of codes["supported_codes"]){
+    for(const sym in data.symbols){
         const option = document.createElement("option");
-        option.value = code[0];
-        option.innerHTML = code[0] + " - " + code[1];
-        $currencySelect.appendChild(option);
+
+        option.value = sym;
+        option.innerHTML = data.symbols[sym].code + " - " + data.symbols[sym].description;
+        
+        $baseCurrencySelect.appendChild(option);
     }
 })().catch(e => console.log(e));
+
 
 async function fillList(){
     $list.innerHTML = "Loading...";
 
-    let selectedType = $currencySelect.value;
+    const selectedType = $baseCurrencySelect.value;
+    const selectedDate = ($date.value).replaceAll("/","-");
 
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/8697e71e86e8d0f6023edebf/latest/${selectedType}`);
+    const response = await fetch(`https://api.exchangerate.host/${selectedDate}?base=${selectedType}`);
     const data = await response.json()
     
-    $list.innerHTML = "";
-    $coinSelect.innerHTML = "";
+    console.log(data);
 
-    for(r in data.conversion_rates){
+    $list.innerHTML = "";
+    $findCurrencySelect.innerHTML = "";
+
+    for(r in data.rates){
         const li = document.createElement("li");
-        li.innerHTML = `<strong>${r}</strong>: <span>${data.conversion_rates[r]}</span>`
+        li.innerHTML = `<strong>${r}</strong>: <span>${data.rates[r]}</span>`
         li.classList = "list-group-item";
         li.id = r;
         
@@ -38,12 +53,28 @@ async function fillList(){
         option.value = r;
         option.innerText = r;
         
-        $coinSelect.appendChild(option);
+        $findCurrencySelect.appendChild(option);
 
         $selectContainer.classList.remove("hidden");
     }
 }
 
+async function executeFillList(){
+    fillList().catch(e => {
+        console.log(e);
+        $list.innerHTML = "An error has ocurred please try again later..."
+    })
+}
+
 function findCoin(){
-    document.querySelector(`#${$coinSelect.value}`).scrollIntoView(); 
+    let obj = document.querySelector(`#${$findCurrencySelect.value}`)
+    obj.scrollIntoView(); 
+    highlight(obj);
+}
+
+function highlight(element){
+    element.classList.add("list-group-item-primary");
+    setTimeout(() => {
+        element.classList.remove("list-group-item-primary");
+    }, 1000);
 }
